6444a4a460e2a1580b1bd14e4677c42c0dbb03c8
Harvey Jones <harveyj@gmail.com>
2014-11-16 14:36:51 -0500

    images

---
+++ images.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"code.google.com/p/go-tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 255, 255)
+}
+func (i Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x), uint8(x), uint8(y), 255}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(m)
+}
