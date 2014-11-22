0b073a6bbbc9dff9ba4afa580e31c0a2fa8bf419
Harvey Jones <harveyj@gmail.com>
2014-11-22 11:26:31 -0500

    images, woo

--- images.go
+++ images.go
@@ -4,9 +4,15 @@ import (
 	"code.google.com/p/go-tour/pic"
 	"image"
 	"image/color"
+	"math/rand"
+	"time"
 )
 
-type Image struct{}
+// Updated to generate a gradient, with a randomly generated R.
+
+type Image struct {
+	seed uint8
+}
 
 func (i Image) ColorModel() color.Model {
 	return color.RGBAModel
@@ -15,10 +21,11 @@ func (i Image) Bounds() image.Rectangle {
 	return image.Rect(0, 0, 255, 255)
 }
 func (i Image) At(x, y int) color.Color {
-	return color.RGBA{uint8(x), uint8(x), uint8(y), 255}
+	return color.RGBA{i.seed, uint8(x), uint8(y), 255}
 }
 
 func main() {
-	m := Image{}
+	rand.Seed(time.Now().UTC().UnixNano())
+	m := Image{uint8(rand.Intn(255))}
 	pic.ShowImage(m)
 }
